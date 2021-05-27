19d5798e6891e71ae3aa0b16b51fffbe475d7582
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2021-05-26 22:14:36 -0500

    added src folder

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./service-account.json");
+var serviceAccount = require("../service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
